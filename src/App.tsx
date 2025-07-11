d3f59fbb07c1da61f516c3568fde0646c2c59c61
Daniil Sudorgin <ddsudorgin@gmail.com>
2025-07-11 18:36:36 +0300

    chore: setup project tooling and CI pipeline

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component<object> {
+  render() {
+    return (
+      <>
+        <h1 className="box text-3xl font-bold underline">Hello world!</h1>
+      </>
+    );
+  }
+}
+
+export default App;
