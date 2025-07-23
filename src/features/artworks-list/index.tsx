45e752ea1ffb928e0802270445fd15b01a5b9873
Daniil Sudorgin <ddsudorgin@gmail.com>
2025-07-23 14:55:13 +0300

    refactor: migrate ArtworksList to functional component

--- src/features/artworks-list/index.tsx
+++ src/features/artworks-list/index.tsx
@@ -1,5 +1,4 @@
 import type { Artwork } from '@/api/artworks-api.types';
-import React from 'react';
 import { Loader } from '../ui/loader';
 import { Card } from '../ui/card';
 import { ErrorMessage } from '../ui/error-message';
@@ -10,38 +9,34 @@ interface ArtworksListProps {
   error: string | null;
 }
 
-export class ArtworksList extends React.Component<ArtworksListProps> {
-  render() {
-    const { items, isLoading, error } = this.props;
-
-    if (isLoading) {
-      return (
-        <div className="flex min-h-[200px] items-center justify-center">
-          <Loader />
-        </div>
-      );
-    }
-
-    if (error) {
-      return (
-        <div className="flex min-h-[200px] items-center justify-center">
-          <ErrorMessage error={error} />
-        </div>
-      );
-    }
-
-    if (items.length === 0) {
-      return <p>No results found for your query.</p>;
-    }
-
+export function ArtworksList({ items, isLoading, error }: ArtworksListProps) {
+  if (isLoading) {
     return (
-      <ul className="grid grid-cols-1 gap-6 py-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {items.map((artwork) => (
-          <li key={artwork.id} className="flex">
-            <Card artwork={artwork} />
-          </li>
-        ))}
-      </ul>
+      <div className="flex min-h-[200px] items-center justify-center">
+        <Loader />
+      </div>
     );
   }
+
+  if (error) {
+    return (
+      <div className="flex min-h-[200px] items-center justify-center">
+        <ErrorMessage error={error} />
+      </div>
+    );
+  }
+
+  if (items.length === 0) {
+    return <p>No results found for your query.</p>;
+  }
+
+  return (
+    <ul className="grid grid-cols-1 gap-6 py-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {items.map((artwork) => (
+        <li key={artwork.id} className="flex">
+          <Card artwork={artwork} />
+        </li>
+      ))}
+    </ul>
+  );
 }
