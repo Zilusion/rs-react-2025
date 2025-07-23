db28e7233496eaa3af0cdf80320d40a30063ec06
Daniil Sudorgin <ddsudorgin@gmail.com>
2025-07-23 17:45:32 +0300

    refactor: migrate App to functional component

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
 import { SearchPage } from './pages/search-page';
 import { ErrorBoundary } from './features/error-boundary';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <ErrorBoundary
-          fallback={
-            <h1 className="p-8 text-center text-red-500">
-              Something went wrong. Please refresh the page.
-            </h1>
-          }
-        >
-          <SearchPage />
-        </ErrorBoundary>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <ErrorBoundary
+        fallback={
+          <h1 className="p-8 text-center text-red-500">
+            Something went wrong. Please refresh the page.
+          </h1>
+        }
+      >
+        <SearchPage />
+      </ErrorBoundary>
+    </>
+  );
 }
 
 export default App;
